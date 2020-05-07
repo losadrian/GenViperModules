bc993df2ae7ffc050afe1d7eb58e129d548b2300
Lakatos Adrián <lakatos.adrian@mav-szk.hu>
2020-05-07 11:55:45 +0200

    Initial Commit

---
+++ GenViperModules/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GenViperModules
+//
+//  Created by Adrian Lakatos on 5/7/20.
+//  Copyright © 2020 LosAdrian. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
